776e5824db0e6ea0b60945d8f0bf706be322e013
MYuuuuuu <944587341@qq.com>
2018-05-21 08:12:20 +0800

    翻译部分界面

--- FFXIVZoomHack/Form1.Designer.cs
+++ FFXIVZoomHack/Form1.Designer.cs
@@ -53,11 +53,12 @@
             // 
             this._autoApplyCheckbox.AutoSize = true;
             this._autoApplyCheckbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this._autoApplyCheckbox.Location = new System.Drawing.Point(6, 25);
+            this._autoApplyCheckbox.Location = new System.Drawing.Point(8, 29);
+            this._autoApplyCheckbox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this._autoApplyCheckbox.Name = "_autoApplyCheckbox";
-            this._autoApplyCheckbox.Size = new System.Drawing.Size(219, 24);
+            this._autoApplyCheckbox.Size = new System.Drawing.Size(174, 29);
             this._autoApplyCheckbox.TabIndex = 0;
-            this._autoApplyCheckbox.Text = "Automatically apply on load";
+            this._autoApplyCheckbox.Text = "加载时自动启用";
             this._autoApplyCheckbox.UseVisualStyleBackColor = true;
             // 
             // _zoomSettingsBox
@@ -72,9 +73,11 @@
             this._zoomSettingsBox.Controls.Add(this._fovLabel);
             this._zoomSettingsBox.Controls.Add(this._zoomLabel);
             this._zoomSettingsBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this._zoomSettingsBox.Location = new System.Drawing.Point(13, 150);
+            this._zoomSettingsBox.Location = new System.Drawing.Point(17, 173);
+            this._zoomSettingsBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this._zoomSettingsBox.Name = "_zoomSettingsBox";
-            this._zoomSettingsBox.Size = new System.Drawing.Size(474, 109);
+            this._zoomSettingsBox.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this._zoomSettingsBox.Size = new System.Drawing.Size(632, 126);
             this._zoomSettingsBox.TabIndex = 1;
             this._zoomSettingsBox.TabStop = false;
             this._zoomSettingsBox.Text = "Zoom Settings";
@@ -87,7 +90,8 @@
             0,
             0,
             131072});
-            this._fovUpDown.Location = new System.Drawing.Point(243, 83);
+            this._fovUpDown.Location = new System.Drawing.Point(324, 96);
+            this._fovUpDown.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this._fovUpDown.Maximum = new decimal(new int[] {
             300,
             0,
@@ -99,7 +103,7 @@
             0,
             131072});
             this._fovUpDown.Name = "_fovUpDown";
-            this._fovUpDown.Size = new System.Drawing.Size(215, 26);
+            this._fovUpDown.Size = new System.Drawing.Size(287, 30);
             this._fovUpDown.TabIndex = 7;
             this._fovUpDown.Value = new decimal(new int[] {
             2,
@@ -109,7 +113,8 @@
             // 
             // _zoomUpDown
             // 
-            this._zoomUpDown.Location = new System.Drawing.Point(10, 83);
+            this._zoomUpDown.Location = new System.Drawing.Point(13, 96);
+            this._zoomUpDown.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this._zoomUpDown.Maximum = new decimal(new int[] {
             1000,
             0,
@@ -121,7 +126,7 @@
             0,
             0});
             this._zoomUpDown.Name = "_zoomUpDown";
-            this._zoomUpDown.Size = new System.Drawing.Size(215, 26);
+            this._zoomUpDown.Size = new System.Drawing.Size(287, 30);
             this._zoomUpDown.TabIndex = 6;
             this._zoomUpDown.Value = new decimal(new int[] {
             2,
@@ -131,41 +136,45 @@
             // 
             // _fovDefaultButton
             // 
-            this._fovDefaultButton.Location = new System.Drawing.Point(362, 45);
+            this._fovDefaultButton.Location = new System.Drawing.Point(483, 52);
+            this._fovDefaultButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this._fovDefaultButton.Name = "_fovDefaultButton";
-            this._fovDefaultButton.Size = new System.Drawing.Size(96, 26);
+            this._fovDefaultButton.Size = new System.Drawing.Size(128, 30);
             this._fovDefaultButton.TabIndex = 5;
-            this._fovDefaultButton.Text = "Default";
+            this._fovDefaultButton.Text = "恢复默认";
             this._fovDefaultButton.UseVisualStyleBackColor = true;
             this._fovDefaultButton.Click += new System.EventHandler(this._fovDefaultButton_Click);
             // 
             // _zoomDefaultButton
             // 
-            this._zoomDefaultButton.Location = new System.Drawing.Point(129, 45);
+            this._zoomDefaultButton.Location = new System.Drawing.Point(172, 52);
+            this._zoomDefaultButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this._zoomDefaultButton.Name = "_zoomDefaultButton";
-            this._zoomDefaultButton.Size = new System.Drawing.Size(96, 26);
+            this._zoomDefaultButton.Size = new System.Drawing.Size(128, 30);
             this._zoomDefaultButton.TabIndex = 4;
-            this._zoomDefaultButton.Text = "Default";
+            this._zoomDefaultButton.Text = "恢复默认";
             this._zoomDefaultButton.UseVisualStyleBackColor = true;
             this._zoomDefaultButton.Click += new System.EventHandler(this._zoomDefaultButton_Click);
             // 
             // _fovLabel
             // 
             this._fovLabel.AutoSize = true;
-            this._fovLabel.Location = new System.Drawing.Point(239, 45);
+            this._fovLabel.Location = new System.Drawing.Point(319, 52);
+            this._fovLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this._fovLabel.Name = "_fovLabel";
-            this._fovLabel.Size = new System.Drawing.Size(99, 20);
+            this._fovLabel.Size = new System.Drawing.Size(52, 25);
             this._fovLabel.TabIndex = 2;
-            this._fovLabel.Text = "Field of View";
+            this._fovLabel.Text = "广角";
             // 
             // _zoomLabel
             // 
             this._zoomLabel.AutoSize = true;
-            this._zoomLabel.Location = new System.Drawing.Point(6, 45);
+            this._zoomLabel.Location = new System.Drawing.Point(8, 52);
+            this._zoomLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this._zoomLabel.Name = "_zoomLabel";
-            this._zoomLabel.Size = new System.Drawing.Size(83, 20);
+            this._zoomLabel.Size = new System.Drawing.Size(52, 25);
             this._zoomLabel.TabIndex = 1;
-            this._zoomLabel.Text = "Zoom Max";
+            this._zoomLabel.Text = "视距";
             // 
             // _processListBox
             // 
@@ -176,79 +185,88 @@
             this._processListBox.Controls.Add(this._processList);
             this._processListBox.Controls.Add(this._autoApplyCheckbox);
             this._processListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this._processListBox.Location = new System.Drawing.Point(13, 50);
+            this._processListBox.Location = new System.Drawing.Point(17, 58);
+            this._processListBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this._processListBox.Name = "_processListBox";
-            this._processListBox.Size = new System.Drawing.Size(474, 94);
+            this._processListBox.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this._processListBox.Size = new System.Drawing.Size(632, 108);
             this._processListBox.TabIndex = 2;
             this._processListBox.TabStop = false;
             this._processListBox.Text = "Processes";
             // 
             // _gotoProcessButton
             // 
-            this._gotoProcessButton.Location = new System.Drawing.Point(285, 57);
+            this._gotoProcessButton.Location = new System.Drawing.Point(380, 66);
+            this._gotoProcessButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this._gotoProcessButton.Name = "_gotoProcessButton";
-            this._gotoProcessButton.Size = new System.Drawing.Size(143, 26);
+            this._gotoProcessButton.Size = new System.Drawing.Size(191, 30);
             this._gotoProcessButton.TabIndex = 8;
-            this._gotoProcessButton.Text = "Bring to Front";
+            this._gotoProcessButton.Text = "返回游戏界面";
             this._gotoProcessButton.UseVisualStyleBackColor = true;
             this._gotoProcessButton.Click += new System.EventHandler(this._gotoProcessButton_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 52);
+            this.label1.Location = new System.Drawing.Point(8, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.Size = new System.Drawing.Size(52, 25);
             this.label1.TabIndex = 8;
-            this.label1.Text = "Processes";
+            this.label1.Text = "进程";
             // 
             // _processList
             // 
             this._processList.FormattingEnabled = true;
-            this._processList.Location = new System.Drawing.Point(95, 55);
+            this._processList.Location = new System.Drawing.Point(127, 63);
+            this._processList.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this._processList.Name = "_processList";
-            this._processList.Size = new System.Drawing.Size(172, 28);
+            this._processList.Size = new System.Drawing.Size(228, 33);
             this._processList.TabIndex = 1;
             // 
             // _updateOffsetsTextbox
             // 
-            this._updateOffsetsTextbox.Location = new System.Drawing.Point(13, 13);
+            this._updateOffsetsTextbox.Location = new System.Drawing.Point(17, 15);
+            this._updateOffsetsTextbox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this._updateOffsetsTextbox.Name = "_updateOffsetsTextbox";
-            this._updateOffsetsTextbox.Size = new System.Drawing.Size(306, 20);
+            this._updateOffsetsTextbox.Size = new System.Drawing.Size(407, 25);
             this._updateOffsetsTextbox.TabIndex = 3;
             // 
             // _updateOffsetsButton
             // 
-            this._updateOffsetsButton.Location = new System.Drawing.Point(404, 12);
+            this._updateOffsetsButton.Location = new System.Drawing.Point(539, 14);
+            this._updateOffsetsButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this._updateOffsetsButton.Name = "_updateOffsetsButton";
-            this._updateOffsetsButton.Size = new System.Drawing.Size(83, 43);
+            this._updateOffsetsButton.Size = new System.Drawing.Size(111, 50);
             this._updateOffsetsButton.TabIndex = 9;
-            this._updateOffsetsButton.Text = "Update Offsets";
+            this._updateOffsetsButton.Text = "更新偏移地址";
             this._updateOffsetsButton.UseVisualStyleBackColor = true;
             this._updateOffsetsButton.Click += new System.EventHandler(this._updateOffsetsButton_Click);
             // 
             // _updateLocationDefault
             // 
-            this._updateLocationDefault.Location = new System.Drawing.Point(325, 14);
+            this._updateLocationDefault.Location = new System.Drawing.Point(433, 16);
+            this._updateLocationDefault.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this._updateLocationDefault.Name = "_updateLocationDefault";
-            this._updateLocationDefault.Size = new System.Drawing.Size(75, 41);
+            this._updateLocationDefault.Size = new System.Drawing.Size(100, 47);
             this._updateLocationDefault.TabIndex = 10;
-            this._updateLocationDefault.Text = "Default";
+            this._updateLocationDefault.Text = "恢复默认";
             this._updateLocationDefault.UseVisualStyleBackColor = true;
             this._updateLocationDefault.Click += new System.EventHandler(this._updateLocationDefault_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(499, 271);
+            this.ClientSize = new System.Drawing.Size(663, 303);
             this.Controls.Add(this._updateLocationDefault);
             this.Controls.Add(this._updateOffsetsButton);
             this.Controls.Add(this._updateOffsetsTextbox);
             this.Controls.Add(this._processListBox);
             this.Controls.Add(this._zoomSettingsBox);
-            this.MaximumSize = new System.Drawing.Size(515, 310);
-            this.MinimumSize = new System.Drawing.Size(515, 310);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximumSize = new System.Drawing.Size(681, 350);
+            this.MinimumSize = new System.Drawing.Size(681, 350);
             this.Name = "Form1";
             this.Text = "FFXIV Zoom Hack";
             this.Load += new System.EventHandler(this.Form1_Load);
